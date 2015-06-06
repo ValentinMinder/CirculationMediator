805e882b4b406bb3773f28811b14413459680fba
ValentinMinder <ValentinMinder@users.noreply.github.com>
2015-06-06 14:37:58 +0200

    traffic light red/green request protocol implemented

--- CirculationMediator/src/colleagues/TrafficLight.java
+++ CirculationMediator/src/colleagues/TrafficLight.java
@@ -6,9 +6,32 @@ import mediator.IMediator;
  * Created by paulnta on 04.06.15.
  */
 public class TrafficLight extends NonMovingColleague {
+	public enum State { GREEN, ORANGE, RED}
+	// stores if a traffic light is green, orange or red.
+	private State current = State.ORANGE;
 
     public TrafficLight(IMediator med) {
         super(med);
     }
+    
+	public boolean isGreen() {
+		return current == State.GREEN;
+	}
+
+	public void green() {
+		current = State.GREEN;
+		mediator.switchMoveEveryOneInArea(this, zone, false);
+	}
+
+	public void red() {
+		current = State.RED;
+		mediator.switchMoveEveryOneInArea(this, zone, true);
+	}
+
+	public void orange() {
+		current = State.ORANGE;
+		// we stop people by security
+		mediator.switchMoveEveryOneInArea(this, zone, true);
+	}
 
 }
--- CirculationMediator/src/mediator/TrafficLightMediator.java
+++ CirculationMediator/src/mediator/TrafficLightMediator.java
@@ -2,7 +2,9 @@ package mediator;
 
 import java.util.Collection;
 
+import colleagues.IColleague;
 import colleagues.TrafficLight;
+import protocol.KeepAliveData;
 import protocol.Zone2D;
 
 /**
@@ -15,17 +17,53 @@ public class TrafficLightMediator extends CirculationMediator {
 	public TrafficLightMediator(GeneralMediator superMediator, Zone2D zone) {
 		super(superMediator, zone);
 	}
+	
+	@Override
+	public void keepAlive(IColleague sender, KeepAliveData data) {
+		super.keepAlive(sender, data);
+		// if the sender is in the area of a traffic light...
+		for (TrafficLight light : lights) {
+			if (data.getZone().isContainedIn(light.getZone())) {
+				requestGreen(light);
+				return;
+			}
+		}
+	}
+	
+	private void makeOtherRed (TrafficLight otherLight ){
+		for (TrafficLight light : lights) {
+			if (light != otherLight) {
+				light.red();
+			}
+		}
+	}
 
 	/**
 	 * Request the green. If successful.
 	 */
 	public void requestGreen(TrafficLight otherLight) {
-		for (TrafficLight light : lights) {
-			if (light != otherLight) {
-					// not okay, dont change
+		
+		if (otherLight.isGreen()) {
+			// okay, already green, dont change ! 
+			// just make sure everyone else is red.
+			makeOtherRed(otherLight);
+			otherLight.green();
+		} else {
+			// request processing: orange.
+			otherLight.orange();
+			for (TrafficLight light : lights) {
+				if (light != otherLight) {
+					if (light.isGreen()) {
+						// at least one other not okay, goes to red.
+						otherLight.red();
+						return;
+					}
+				}
 			}
+			// okay, change green, just make others red.
+			makeOtherRed(otherLight);
+			otherLight.green();
 		}
-		// okay, change grren
 	}
 
 }
