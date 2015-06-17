3384c06a228eb263bc0ae2d0c162bcdd1cbda9da
ValentinMinder <ValentinMinder@users.noreply.github.com>
2015-06-18 01:27:14 +0200

    traffic light with time

--- CirculationMediator/src/colleagues/TrafficLight.java
+++ CirculationMediator/src/colleagues/TrafficLight.java
@@ -1,11 +1,15 @@
 package colleagues;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import mediator.IMediator;
 
 /**
  * Created by paulnta on 04.06.15.
  */
 public class TrafficLight extends NonMovingColleague {
+	private Timer timer = new Timer();
 	public enum State { GREEN, ORANGE, RED}
 	// stores if a traffic light is green, orange or red.
 	private State current = State.ORANGE;
@@ -26,12 +30,30 @@ public class TrafficLight extends NonMovingColleague {
             return current == State.RED;
         }
 
+        private TimerTask tt;
 	public void green() {
+		if (current != State.GREEN) {
+			// on redémarre tout le monde dans la zone.
+			mediator.switchMoveEveryOneInArea(this, zone, false);
+		}
 		current = State.GREEN;
-		mediator.switchMoveEveryOneInArea(this, zone, false);
+		if (tt != null) {
+			tt.cancel();
+		}
+		tt = new TimerTask() {
+			
+			@Override
+			public void run() {
+				orange();
+			}
+		};
+		timer.schedule(tt, 1000 * 5); // green for 5 seconds
 	}
 
 	public void red() {
+		if (tt != null) {
+			tt.cancel();
+		}
 		current = State.RED;
 		mediator.switchMoveEveryOneInArea(this, zone, true);
 	}
